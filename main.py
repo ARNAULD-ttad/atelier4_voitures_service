3b5524c821d5b95389da72fa537b36d483770aba
arnauld <arnoldtchumno@gmail.com>
2026-03-12 16:01:45 -0400

    Ajout méthode afficher_info dans Employe

--- main.py
+++ main.py
@@ -5,3 +5,13 @@ class Employe:
         self.nom = nom
         self.poste = poste
         self.voiture = None
+
+    def afficher_info(self):
+
+        print("Employé :", self.nom)
+        print("Poste :", self.poste)
+
+        if self.voiture is not None:
+            print("Voiture :", self.voiture.marque, self.voiture.modele)
+        else:
+            print("Aucune voiture attribuée")
